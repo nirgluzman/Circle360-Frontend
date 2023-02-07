8119d42f79da9391981a4c328212158f81c43616
nirgluzman <nir.gluzman@gmail.com>
2023-02-07 23:13:21 +0100

    add Auth component

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,15 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "circle360frontend": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+// ********** Amplify **********
+import { ThemeProvider } from "@aws-amplify/ui-react";
 import { Amplify } from "aws-amplify";
-import awsExports from "./aws-exports";
-Amplify.configure(awsExports);
+
+import awsconfig from "./aws-exports";
+
+import "@aws-amplify/ui-react/styles.css";
+import "@fontsource/inter/variable.css";
+import { studioTheme } from "./ui-components";
+
+Amplify.configure(awsconfig);
+// ********** Amplify **********
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <ThemeProvider theme={studioTheme}>
+    <App />
+  </ThemeProvider>
+);
